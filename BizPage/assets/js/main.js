8bde386ee5f559aa8603340b92b6d41ba19a235e
sampadadhole <sampadadhole4@gmail.com>
2022-01-04 01:08:50 +0530

    fourth commit

--- BizPage/assets/js/main.js
+++ BizPage/assets/js/main.js
@@ -313,4 +313,19 @@
     })
   });
 
+  function sendMail(str) {
+    var link = "mailto:slim.hmidi1@gmail.com" + "&subject=" + escape("This is my subject") + "&body=" + escape(str);
+    location.href = link;
+}
+function submit_comment() {
+var name = document.forms["contact_form"]["Name"].value;
+var Email = document.forms["contact_form"]["Email"].value;
+var Phone = document.forms["contact_form"]["Phone"].value;
+var Location = document.forms["contact_form"]["Location"].value;
+var Comment = document.forms["contact_form"]["Location"].value;
+if((name !="") && (Email !="") && (Phone !="") && (Location !="") && (Comment !="")) {
+   sendMail("HELLO");
+}
+}
+
 })()
